ce25bcbbf9801278c7ea8e159a24afdac38048cc
jahedprince <jahedprince183@gmail.com>
2023-08-08 03:02:31 -0400

    all edits

---
+++ README.md
@@ -0,0 +1,23 @@
+<h2>Jahed's Portfolio</h2>
+
+<strong>:crown: advantages</strong>
+
+<img src="https://img.shields.io/badge/-mobile friendly-blue" alt="mobile friendly"/> &nbsp; <img src="https://img.shields.io/badge/-light/dark mode-blue" alt="light/dark mode"/> &nbsp; <img src="https://img.shields.io/badge/-json fetched data-blue" alt="json fetched data"/> &nbsp; <img src="https://img.shields.io/badge/-minimalistic-blue" alt="minimalistic"/> &nbsp; <img src="https://img.shields.io/badge/-expandable-blue" alt="expandable"/>
+
+<br/>
+
+<h3>:eye_speech_bubble: Live demo</h3>
+
+Check live demonstration <a href="https://jahedprince.github.io/Portfolio/"><strong>here</strong></a>
+
+![Portfolio Preview](public/images/portfolio/preview.png)
+
+<h3>:star: Inspirations</h3>
+
+<a href="https://github.com/stephane-monnot/react-vertical-timeline">React Vertical Timeline</a> <br/>
+<a href="https://github.com/rcaferati/react-awesome-slider">React Awesome Slider</a> <br/>
+<a href="https://github.com/markusenglund/react-switch">React Switch</a> <br/>
+<a href="https://github.com/catalinmiron/react-typical">React Typical</a> <br/>
+<a href="https://iconify.design/icon-sets/?query=angular">Iconify Design</a> <br/>
+<a href="https://www.w3docs.com/snippets/css/how-to-create-polaroid-image-with-css.html#">Polaroid effect</a> <br/>
+<a href="https://tholman.com/github-corners/">GitHub Ref Corner</a>
---
+++ src/components/Photography.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+class Photography extends Component {
+  render() {
+    if (this.props.resumePhotography || this.props.resumeBasicInfo) {
+      var sectionName = this.props.resumeBasicInfo.section_name.photography;
+      var photographyProjects = this.props.resumePhotography.map(function (
+        project
+      ) {
+        return (
+          <div
+            className="col-sm-12 col-md-6 col-lg-4"
+            key={project.title}
+            style={{ cursor: "pointer" }}
+          >
+            <span className="portfolio-item d-block">
+              <div className="travelfoto">
+                <div>
+                  <img
+                    src={project.images[0]}
+                    alt="projectImages"
+                    height="230"
+                    style={{
+                      marginBottom: 0,
+                      paddingBottom: 0,
+                      position: "relative",
+                    }}
+                  />
+                  <span className="project-date">{project.date}</span>
+                  <br />
+                  <p className="project-title-settings mt-3">{project.title}</p>
+                  <p>{project.description}</p>
+                </div>
+              </div>
+            </span>
+          </div>
+        );
+      });
+    }
+
+    return (
+      <section id="photography">
+        <div className="col-md-12">
+          <h1 className="section-title" style={{ color: "black" }}>
+            <span>{sectionName}</span>
+          </h1>
+          <div className="col-md-12 mx-auto">
+            <div className="row mx-auto">{photographyProjects}</div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Photography;
